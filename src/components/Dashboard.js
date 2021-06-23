c812f5fba3e05131632c2ee9a0e12933cd806368
Nguyen, Victor <22437776+victornguyen75@users.noreply.github.com>
2021-06-22 23:35:47 -0500

    feat(Dashboard.js): add search functionality and page setup

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,12 +1,59 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import useAuth from "./useAuth";
+import { Container, Form } from "react-bootstrap";
+import SpotifyWebApi from "spotify-web-api-node"
+
+const spotifyApi = new SpotifyWebApi({
+  clientId: "e3c52dc073bb460cbabfabfdf10c4463",
+})
 
 export default function Dashboard({ code }) {
   const accessToken = useAuth(code);
+  const [search, setSearch] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+  console.log(searchResults)
+
+  useEffect(() => {
+    if (!accessToken) return
+    spotifyApi.setAccessToken(accessToken)
+  }, [accessToken])
+
+  useEffect(() => {
+    if (!search) return setSearchResults([])
+    if (!accessToken) return setSearchResults
+
+    spotifyApi.searchTracks(search).then((res) => {
+      setSearchResults(res.body.tracks.items.map((track) => {
+      const smallestAlbumImage = track.album.images.reduce(
+        (smallest, image) => {
+          if (image.height < smallest.height) return image;
+          return smallest
+      }, track.album.images[0])
+
+      return {
+        artist: track.artists[0].name,
+        title: track.name,
+        uri: track.uri,
+        albumUrl: smallestAlbumImage.url
+      }})
+    )})
+  }, [search, accessToken])
+
   return (
-    <>
-      <h1>{code}</h1>
-      <h2>{accessToken}</h2>
-    </>
+    <Container
+      className="d-flex flex-column py-2"
+      style={{height: "100vh"}}
+    >
+      <Form.Control
+        type="search"
+        placeholder="Search Songs/Artists"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <div className="flex-grow-1 my-2" style={{ overflowY: "auto"}}>
+        Songs
+      </div>
+      <div>Bottom</div>
+    </Container>
   );
 }
