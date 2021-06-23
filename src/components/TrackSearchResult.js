a74ab23a17d7b8adff28c11ccdc1b91e22d75f86
Nguyen, Victor <22437776+victornguyen75@users.noreply.github.com>
2021-06-22 23:51:26 -0500

    feat(TrackSearchResults): make the search result more appealing

--- src/components/TrackSearchResult.js
+++ src/components/TrackSearchResult.js
@@ -1,11 +1,20 @@
 import React from 'react'
 
 export default function TrackSearchResult({ track }) {
+  const handlePlay = () => {
+    // chooseTrack(track)
+    console.log(track.title)
+  }
   return (
-    <div className="d-flex m-2 align-items-center">
+    <div
+      className="d-flex m-2 align-items-center"
+      style={{ cursor: "pointer" }}
+      onClick={handlePlay}
+    >
       <img src={track.albumUrl} style={{ height: "64px", width: "64px" }} alt={`${track.name} album`}/>
       <div className="ml-3">
         <div>{track.title}</div>
+        <div className="text-muted">{track.artist}</div>
       </div>
     </div>
   )
