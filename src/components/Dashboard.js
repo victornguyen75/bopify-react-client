9daaaf0c1092441d48429b8b2fcffe1270ff0dc2
Nguyen, Victor <22437776+victornguyen75@users.noreply.github.com>
2021-06-23 18:33:57 -0500

    feat(Dashboard.js, Player.js, TrackSearchResult.js): add music player

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,66 +1,81 @@
 import React, { useState, useEffect } from "react";
 import useAuth from "./useAuth";
 import { Container, Form } from "react-bootstrap";
-import SpotifyWebApi from "spotify-web-api-node"
-import TrackSearchResult from "./TrackSearchResult"
+import SpotifyWebApi from "spotify-web-api-node";
+import TrackSearchResult from "./TrackSearchResult";
+import Player from "./Player";
 
 const spotifyApi = new SpotifyWebApi({
   clientId: "e3c52dc073bb460cbabfabfdf10c4463",
-})
+});
 
 export default function Dashboard({ code }) {
   const accessToken = useAuth(code);
   const [search, setSearch] = useState("");
   const [searchResults, setSearchResults] = useState([]);
+  const [playingTrack, setPlayingTrack] = useState();
 
   useEffect(() => {
-    if (!accessToken) return
-    spotifyApi.setAccessToken(accessToken)
-  }, [accessToken])
+    if (!accessToken) return;
+    spotifyApi.setAccessToken(accessToken);
+  }, [accessToken]);
 
   useEffect(() => {
-    if (!search) return setSearchResults([])
-    if (!accessToken) return setSearchResults
+    if (!search) return setSearchResults([]);
+    if (!accessToken) return setSearchResults;
 
     let cancel = false;
     spotifyApi.searchTracks(search).then((res) => {
       if (cancel) return;
 
-      setSearchResults(res.body.tracks.items.map((track) => {
-      const smallestAlbumImage = track.album.images.reduce(
-        (smallest, image) => {
-          if (image.height < smallest.height) return image;
-          return smallest
-      }, track.album.images[0])
+      setSearchResults(
+        res.body.tracks.items.map((track) => {
+          const smallestAlbumImage = track.album.images.reduce(
+            (smallest, image) => {
+              if (image.height < smallest.height) return image;
+              return smallest;
+            },
+            track.album.images[0]
+          );
 
-      return {
-        artist: track.artists[0].name,
-        title: track.name,
-        uri: track.uri,
-        albumUrl: smallestAlbumImage.url
-      }})
-    )})
+          return {
+            artist: track.artists[0].name,
+            title: track.name,
+            uri: track.uri,
+            albumUrl: smallestAlbumImage.url,
+          };
+        })
+      );
+    });
 
-    return () => (cancel = true)
-  }, [search, accessToken])
+    return () => (cancel = true);
+  }, [search, accessToken]);
+
+  const chooseTrack = (track) => {
+    setPlayingTrack(track);
+    setSearch("");
+  };
 
   return (
-    <Container
-      className="d-flex flex-column py-2"
-      style={{height: "100vh"}}
-    >
+    <Container className="d-flex flex-column py-2" style={{ height: "100vh" }}>
       <Form.Control
         type="search"
         placeholder="Search Songs/Artists"
         value={search}
         onChange={(e) => setSearch(e.target.value)}
       />
-      <div className="flex-grow-1 my-2" style={{ overflowY: "auto"}}>
+      <div className="flex-grow-1 my-2" style={{ overflowY: "auto" }}>
         {searchResults.map((track) => (
-          <TrackSearchResult track={track} key={track.uri}/>
+          <TrackSearchResult
+            key={track.uri}
+            track={track}
+            chooseTrack={chooseTrack}
+          />
         ))}
       </div>
-      <div>Bottom</div>
+      <div>
+        <Player accessToken={accessToken} trackUri={playingTrack?.uri} />
+      </div>
     </Container>
   );
 }
