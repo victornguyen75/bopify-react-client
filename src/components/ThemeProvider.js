69c27cc1d13b4d07e47ffc783e71fc909959337e
Nguyen, Victor <22437776+victornguyen75@users.noreply.github.com>
2021-06-23 23:23:54 -0500

    styles(ThemeProvider.js): set up provider

---
+++ src/components/ThemeProvider.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect, createContext } from "react";
+import darkTheme from "./theme/darkTheme";
+import lightTheme from "./theme/lightTheme";
+export const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState("light");
+
+  useEffect(() => {
+    let theme = window.localStorage.getItem("theme");
+    if (!theme) {
+      theme = "light";
+      window.localStorage.setItem("theme", theme);
+    }
+    setTheme(theme);
+  }, []);
+
+  const toggleTheme = () => {
+    if (theme === "light") {
+      setTheme("dark");
+      window.localStorage.setItem("theme", "dark");
+    } else {
+      setTheme("light");
+      window.localStorage.setItem("theme", "light");
+    }
+  };
+
+  return (
+    <ThemeContext.Provider
+      value={{ theme: theme === "light" ? lightTheme : darkTheme, toggleTheme }}
+    >
+      {children}
+    </ThemeContext.Provider>
+  );
+};
